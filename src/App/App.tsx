7c201e60223ce9301b260d0555f163675e34aa9d
SLain <sl163@ya.ru>
2021-06-14 16:39:22 +0300

    Добавляет анимацию для роутинга страниц и анимацию титульного экрана в компоненте Welcome;

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import Header from '../components/Header';
 import Welcome from '../components/Welcome';
 import LangChanger from '../components/LangChanger';
 import Projects from '../features/projects';
 
+import classes from './App.module.scss';
+
 const App: React.FC = () => {
+    const routes = [
+        { path: '/', name: 'Welcome', Component: Welcome },
+        { path: '/works', name: 'Projects', Component: Projects },
+    ];
+
+    const RouteList = routes.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+            {({ match }) => (
+                <CSSTransition
+                    in={match != null}
+                    timeout={300}
+                    classNames={{
+                        enter: classes.mainBlockEnter,
+                        enterActive: classes.mainBlockEnterActive,
+                        exit: classes.mainBlockExit,
+                        exitActive: classes.mainBlockExitActive,
+                    }}
+                    unmountOnExit
+                >
+                    <div className='mainBlock'>
+                        <Component />
+                    </div>
+                </CSSTransition>
+            )}
+        </Route>
+    ));
+
     return (
         <>
             <Route path='*' component={Header} />
-            <Route path='*' component={LangChanger} />
-            <Route path='/' component={Welcome} exact />
-            <Route path='/works' component={Projects} exact />
+            <LangChanger />
+            {RouteList}
         </>
     );
 };
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { CSSTransition } from 'react-transition-group';
 
 import classes from './Welcome.module.scss';
 import pointer from './pointer.svg';
 
 const Welcome: React.FC = () => {
     const { t } = useTranslation();
+    const [displayInfo, setDisplayInfo] = useState(false);
+
+    const dataForTitleList = [
+        <p className={classes.helloWorld}>hello, world.</p>,
+        <h1 className={classes.title}>{t`welcome.name`}</h1>,
+        <h2 className={classes.subTitleBlock}>
+            <span className={classes.subTitle}>front-end developer</span>
+            <span className={classes.subTitle}>react developer</span>
+        </h2>,
+    ];
+    const doneTitleList = dataForTitleList.map((element, i) => (
+        <li key={i} className={classes.test}>
+            <CSSTransition
+                in={displayInfo}
+                timeout={1000}
+                classNames={{
+                    enter: classes.titleItemEnter,
+                    enterActive: classes.titleItemEnterActive,
+                }}
+                mountOnEnter
+            >
+                {element}
+            </CSSTransition>
+        </li>
+    ));
+
+    useEffect(() => {
+        setTimeout(() => setDisplayInfo(true), 50);
+    });
 
     return (
         <section className={classes.container}>
-            <p className={classes.helloWorld}>hello, world.</p>
-            <h1 className={classes.title}>{t`welcome.name`}</h1>
-            <h2 className={classes.subTitleBlock}>
-                <span className={classes.subTitle}>front-end developer</span>
-                <span className={classes.subTitle}>react developer</span>
-            </h2>
-            <Link className={[classes.linkBtn, 'btn'].join(' ')} to='/works'>
-                {t`welcome.myWorks`}
-                <img src={pointer} alt='pointer' width={20} height={20} className={classes.arrow} />
-            </Link>
+            <ul className={classes.titleList}>{doneTitleList}</ul>
+            <CSSTransition
+                in={displayInfo}
+                timeout={1000}
+                classNames={{
+                    enter: classes.imgEnter,
+                    enterActive: classes.imgEnterActive,
+                }}
+                mountOnEnter
+            >
+                <Link
+                    className={[classes.linkBtn, 'btn'].join(' ')}
+                    to='/works'
+                >
+                    {t`welcome.myWorks`}
+                    <img
+                        src={pointer}
+                        alt='pointer'
+                        width={20}
+                        height={20}
+                        className={classes.arrow}
+                    />
+                </Link>
+            </CSSTransition>
         </section>
     );
 };
