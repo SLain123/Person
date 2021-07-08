ca570abf9e62ec511c6fe948e9627f54c7d831f8
SLain <sl163@ya.ru>
2021-07-08 18:01:24 +0300

    Чистить лишние шаблоны кода; Добавляет адаптив к компоненту Project and WorkItem;

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,19 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import projectsReducer from '../features/projects/projectsSlice';
 import aboutReducer from '../features/about/aboutSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    projects: projectsReducer,
-    about: aboutReducer
-  },
+    reducer: {
+        projects: projectsReducer,
+        about: aboutReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
