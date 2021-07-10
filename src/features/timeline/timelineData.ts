d23ef4c4435bffc2fffae656a6cfdaee9e8a6f8d
SLain <sl163@ya.ru>
2021-07-10 12:38:29 +0300

    Добавляет адаптив для компонентов Contact, ContactItem, TimeTable, TimeCell; Добавляет немного данных в список компонента timelineData;

--- src/features/timeline/timelineData.ts
+++ src/features/timeline/timelineData.ts
@@ -2,6 +2,7 @@ import { Tech } from '../../types/timelineTypes';
 
 export const techData: Tech[] = [
     { techName: 'React Transition Group', done: true },
+    { techName: 'Next JS', done: false },
     { techName: 'Gatsby JS', done: false },
     { techName: 'MobX', done: false },
 ];
