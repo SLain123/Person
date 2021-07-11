3e7116c2774837aff66ceff59c88ec94789a64c7
SLain <sl163@ya.ru>
2021-07-11 20:02:22 +0300

    Добавляет lazy load для изображений в contact;

--- src/features/contact/Contact.tsx
+++ src/features/contact/Contact.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import ContactItem from '../../components/ContactItem';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import classes from './Contact.module.scss';
 import constactImg from './contactList.jpg';
+import spin from '../../components/Spinner/spin.gif';
 
 const Concact: React.FC = () => {
     const { t } = useTranslation();
@@ -33,12 +35,13 @@ const Concact: React.FC = () => {
         <div className={classes.contactBlock}>
             <p className={classes.contactTitle}>{t`contact.title`}</p>
             <ul className={classes.contactList}>{contactList}</ul>
-            <img
+            <LazyLoadImage
                 src={constactImg}
                 alt='just contact stub'
                 className={classes.contactImg}
                 width={320}
                 height={110}
+                placeholderSrc={spin}
             />
         </div>
     );
